d491071742760da846ea746096133b7b4d84930d
Jack Wilburn <jackwilburn@tutanota>
2018-11-01 14:51:05 -0600

    Update run.sh to have a more fleshed out body

--- run.sh
+++ run.sh
@@ -3,21 +3,24 @@ touch /var/www/html/index.html
 touch /var/www/html/A/A.html
 
 echo "<!DOCTYPE HTML>" > /var/www/html/index.html
-echo "<head>
+echo '<head>
 	<title>Welcome To Index</title>
 </head>
 
 <body>
 	<h1>This page is called index.txt</h1>
-	<a href="A/A.html">Here is a link to A.html</a>
-</body>" >> /var/www/html/index.html
+	<a href=A/A.html>Here is a link to A.html</a>
+	<p>Here is a picture of me</p>
+	<img src="https://avatars2.githubusercontent.com/u/36867477?s=460&v=4"></img>
+</body>' >> /var/www/html/index.html
 
-echo "<!DOCTYPE HTML>" > /var/www/html/A/A.html
-echo "
+echo '<!DOCTYPE HTML>' > /var/www/html/A/A.html
+echo '
 <head>
-	<title>Welcome To Index</title>
+	<title>Welcome To A</title>
 </head>
 
 <body>
 	<h1>This page is called A.html</h1>
-</body>" >> /var/www/html/A/A.html
+	<p>It is beautiful, you must admit</p>
+</body>' >> /var/www/html/A/A.html
